5717193421c57c64b4a1c6dad7431a406db4542e
mahsa <mahsawz@gmail.com>
2019-01-08 16:11:33 +0000

    Update kernel.cpp (Add keyboardDriver)

--- kernel.cpp
+++ kernel.cpp
@@ -1,6 +1,7 @@
     #include "types.h"
     #include "gdt.h"
     #include "interrupts.h"
+   #include "keyboard.h"
    
     void printf(char* str)
 	{
@@ -57,6 +58,7 @@
 	    printf("Hello World! --- http://www.AlgorithMan.de");
 	    GlobalDescriptorTable gdt;
 	    InterruptManager interrupts(0x20, &gdt);
+	    KeyboardDriver keyboard(&interrupts);
 	    interrupts.Activate();
 	
 	    while(1);
